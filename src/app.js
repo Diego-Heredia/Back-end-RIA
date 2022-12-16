d3e4ab979a6ff10166e10d2a07b0e95f94176812
Diego Heredia <diego@MacBook-Pro-de-Diego.local>
2022-12-15 19:11:35 -0600

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 import employeesRoutes from "./routes/employees.routes.js";
 import indexRoutes from "./routes/index.routes.js";
@@ -9,6 +10,11 @@ const app = express();
 // Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // Routes
 app.use("/", indexRoutes);
